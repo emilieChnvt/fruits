1d240b972f0fa1df78023b9c601627f23e44faa0
Emilie <emilie.chnvt@gmail.com>
2025-04-15 21:04:05 +0200

    delete method for fruits added

--- src/Controller/FruitsController.php
+++ src/Controller/FruitsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Fruits;
 use App\Repository\FruitsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,5 +26,13 @@ final class FruitsController extends AbstractController
         return $this->render('fruits/show.html.twig', [
                'fruits' => $fruits,
         ]);
+    }
+
+    #[Route('/fruits/delete/{id}', name: 'delete_fruit')]
+    public function delete(FruitsRepository $fruitsRepository, EntityManagerInterface $manager, Fruits $fruits): Response
+    {
+        $manager->remove($fruits);
+        $manager->flush();
+        return $this->redirectToRoute('fruits');
     }
 }
