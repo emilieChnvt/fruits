473e36afe8a920ca7607847b1531cc2dfaad129e
Emilie <emilie.chnvt@gmail.com>
2025-04-15 20:52:54 +0200

    index method for fruits added

---
+++ src/Controller/FruitsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Fruits;
+use App\Repository\FruitsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FruitsController extends AbstractController
+{
+    #[Route('/fruits', name: 'fruits')]
+    public function index(FruitsRepository $fruitsRepository): Response
+    {
+        $fruits = $fruitsRepository->findAll();
+        return $this->render('fruits/index.html.twig', [
+            'fruits' => $fruits,
+        ]);
+    }
+
+    #[Route('/fruits/show/{id}', name: 'show_fruits')]
+    public function show(Fruits $fruits): Response
+    {
+        return $this->render('fruits/show.html.twig', [
+               'fruits' => $fruits,
+        ]);
+    }
+}
